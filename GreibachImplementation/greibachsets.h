b3eb4d31bb4f503f7bc9e7eb3f363912b6a75105
Nikolaos Anastasopoulos <nikosmaiden@gmail.com>
2019-03-27 11:44:59 +0200

    Finished GreibachSets

--- GreibachImplementation/greibachsets.h
+++ GreibachImplementation/greibachsets.h
@@ -3,7 +3,32 @@
 
 #include "enumterminalsmap.h"
 
-// This class represents
+// This class represents is a container of the boundaries of the sets (S,N,T) of a formal
+// grammar in the Greibach Normal Form. The boundaries are saved in an enumerated form to
+// be later used by the algorithm. The original terminal symbols are stored in an instance
+// of the EnumTerminalsMap class.
+//
+// * S <- Starting symbol
+// * N <- Non-Terminals set
+// * T <- Terminals set
+//
+// * Random selections from the sets are implemented for initialization of the population
+// * pool.
+//
+// Example: Consider a grammar with set sizes TSize (Terminals) and NTSize (Non-Terminals)
+//
+// The enumeration boundaries are created as:
+// +--------+-----------+----------------+
+// | Set    | From      | To             |
+// +--------+-----------+----------------+
+// | S      |      0    |              0 |
+// +--------+-----------+----------------+
+// | T      |      1    |          TSize |
+// +--------+-----------+----------------+
+// | N      | TSize + 1 | TSize + NTsize |
+// +--------+-----------+----------------+
+//
+// The above table implies that a set S = [From, To] (not [From, To)!).
 
 class GreibachSets
 {
