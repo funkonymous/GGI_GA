4517534964ce7abd481840bc025419e29f0eeb14
Nikolaos Anastasopoulos <nikosmaiden@gmail.com>
2019-06-20 15:53:03 +0300

    Make intersection of experimental datasets NULL

--- DataFromRegex/generateRegexDataSets.py
+++ DataFromRegex/generateRegexDataSets.py
@@ -10,13 +10,13 @@ if not os.path.exists(directory) :
 PosFileAstart = open(directory+'/'+"PosData.txt","w")
 
 for i in range(299) :
-    word = exrex.getone('[a-f]{6,7}[a-c]{1,3}[a-f]{4,7}[a-c]{1,3}')
+    word = exrex.getone('[a-f]{6,7}[a-c]{2,3}[a-f]{4,7}[a-c]{1,3}')
     for i in range(len(word)) :
         PosFileAstart.write('(')
         PosFileAstart.write(word[i])
         PosFileAstart.write(')')
     PosFileAstart.write('\n')
-word = exrex.getone('[a-f]{6,7}[a-c]{1,3}[a-f]{4,7}[a-c]{1,3}')
+word = exrex.getone('[a-f]{6,7}[a-c]{2,3}[a-f]{4,7}[a-c]{1,3}')
 for i in range(len(word)) :
     PosFileAstart.write('(')
     PosFileAstart.write(word[i])
@@ -27,13 +27,13 @@ PosFileAstart.close()
 PosFileBstart = open(directory+'/'+"NegData.txt","w")
 
 for i in range(299) :
-    word = exrex.getone('[a-f]{6,7}[d-f]{1,3}[a-f]{4,7}[d-f]{1,3}')
+    word = exrex.getone('[a-f]{6,7}[d-f]{2,3}[a-f]{4,7}[d-f]{1,3}')
     for i in range(len(word)) :
         PosFileBstart.write('(')
         PosFileBstart.write(word[i])
         PosFileBstart.write(')')
     PosFileBstart.write('\n')
-word = exrex.getone('[a-f]{6,7}[d-f]{1,3}[a-f]{4,7}[d-f]{1,3}')
+word = exrex.getone('[a-f]{6,7}[d-f]{2,3}[a-f]{4,7}[d-f]{1,3}')
 for i in range(len(word)) :
     PosFileBstart.write('(')
     PosFileBstart.write(word[i])
